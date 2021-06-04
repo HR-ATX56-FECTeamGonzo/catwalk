36d7deb72da78481a89aff1b744887603f09d636
Jacob Ward <smilinjake@gmail.com>
2021-06-04 12:07:52 -0500

    set up react hooks and created button element

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import ReviewsList from './ratings_reviews/ReviewsList.jsx';
+import ProductInfo from './overview/ProductInfo.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (<div>react app</div>);
+    return (
+      <div>react app
+        <ReviewsList />
+      </div>
+    );
   }
 }
 
