cb1cd3406e4b911fca23c4f940aae4397e547a37
psohn12 <patricksohn12@gmail.com>
2021-06-08 19:41:54 -0500

    bless this mess

---
+++ client/src/components/ratings_reviews/NewReview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NewReview = () => {
+  return (
+    <div>
+      <form>
+        <input>name</input>
+        <input>title</input>
+        <input>body</input>
+        <input>helpful</input>
+        <imput>rating</imput>
+      </form>
+    </div>
+  );
+};
+
+export default NewReview;
