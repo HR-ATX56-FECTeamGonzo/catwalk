dff55ef4641b65a48a85adb32aa41d0fa5af143d
Jacob Ward <smilinjake@gmail.com>
2021-06-08 19:14:26 -0500

    made a headers variable for get requests

--- client/src/components/ratings_reviews/RatingsAndReviews.js
+++ client/src/components/ratings_reviews/RatingsAndReviews.js
@@ -17,13 +17,13 @@ const RatingsAndReviews = () => {
   const [count, setCount] = useState(2);
   const [sort, setSort] = useState('');
   const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hratx/reviews/';
-
+  const headers = { headers: { 'Authorization': `${GITHUB_API_KEY}`}}
   // state are needed for the sidebar component
   const [metaData, setMetaData] = useState({});
 
   // getting reviews for a product
   const getReviews = () => {
-    axios.get(`${url}?count=1000&sort=${sort}&product_id=${exampleData.id}`, { headers: { 'Authorization': `${GITHUB_API_KEY}`}})
+    axios.get(`${url}?count=1000&sort=${sort}&product_id=${exampleData.id}`, headers)
       .then((response) => {
         // console.log(response.data.results);
         let dataArr = response.data.results;
@@ -36,7 +36,7 @@ const RatingsAndReviews = () => {
 
   // a method for fetching metadata for products is needed to send to SideBar component
   const getMetaData = () => {
-    axios.get(`${url}meta?product_id=${exampleData.id}`, { headers: { 'Authorization': `${GITHUB_API_KEY}`}})
+    axios.get(`${url}meta?product_id=${exampleData.id}`, headers)
       .then((response) => {
         // console.log('this is results from metadata fetch', response.data);
         setMetaData(response.data);
