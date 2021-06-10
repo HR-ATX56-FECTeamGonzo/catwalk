4afda259982e381c6932f57bdd792034ea8acc27
Angela Wu <ange.y.wu@gmail.com>
2021-06-10 11:45:07 -0500

    progress so far

--- client/src/redux-helpers/related/reduxOutfitList.js
+++ client/src/redux-helpers/related/reduxOutfitList.js
@@ -1,1 +1,43 @@
-//add action creators, reducers and containers here
+import axios from 'axios';
+import GITHUB_API_KEY from '../../config/config.js';
+import { useDispatch, useSelector } from 'react-redux';
+import exampleData from '../../store/exampleData.js';
+
+//action creators
+export const addOufit = (outfitsArray) => ({
+  type: 'ADD_OUTFIT',
+  outfitsArray
+});
+
+//
+//
+//need to pass in parameter 'productId' and replace exampleData.id
+export const getRelatedProducts = () => {
+  let url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/';
+  // let currentProductId = useSelector(state => state.currentProduct.id);
+
+  return axios.get(`${url}products/${exampleData.id}/related`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}`}})
+    .then((response) => {
+      console.log(response);
+      let relatedProductsArray = [];
+      for (var i = 0; i < response.data; i++) {
+        relatedProductsArray.push(axios.get(`${url}products/${response.data[i]}`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}`}}));
+      }
+      return Promise.all(relatedProductsArray);
+    })
+    .then((result) => {
+      console.log(result);
+      dispatch(addRelatedProducts(result.data));
+    })
+    .catch(() => console.log('there has been an error fetching the metadata for related products'));
+};
+
+//reducers
+export const relatedProductsReducer = (previousState = [], action) => {
+  switch (action.type) {
+  case 'ADD_RELATED_PRODUCTS':
+    return action.relatedProductsArray;
+  default:
+    return previousState;
+  }
+};
