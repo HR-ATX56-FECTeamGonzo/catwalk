5f43860bacf75560ed59a0b4b5e036d5f9ad0fc3
psohn12 <patricksohn12@gmail.com>
2021-06-09 20:26:32 -0500

    current image properly centers

--- client/src/components/overview/components/ImageGallery.js
+++ client/src/components/overview/components/ImageGallery.js
@@ -1,14 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import {Box, Grid, Container} from '@material-ui/core';
+import { sizing, borders, spacing, flexbox } from '@material-ui/system';
 
 const ImageGallery = ({photos, index}) => {
-  // state saves
-  const [] = useState({});
+  // state for currently displayed image that's instantiated with index prop
+  const [currentIndex, setIndex] = useState(index);
+
   // default view should be 50-60% width of page, or minimum certain amount
-
+  // fixed height of image
   return (
-    <div id='gallery'>
+    <Box display='flex' border={1} justifyContent='center' height='500px' width='50%' className='gallery'>
+      {/* mess with height/width to get this image to scale correctly with browser size*/}
+      <Box component='img' height='auto' width='auto' maxWidth='100%' src={photos[currentIndex].url}/>
 
-    </div>
+    </Box>
   );
 };
 
