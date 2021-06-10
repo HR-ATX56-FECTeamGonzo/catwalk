f54f5cd6a196833c817c460575f39f0c84172fa6
Jacob Ward <smilinjake@gmail.com>
2021-06-09 20:06:49 -0500

    implementing grid and styling for everything in sidebar but characteristics

--- client/src/components/ratings_reviews/SideBar.js
+++ client/src/components/ratings_reviews/SideBar.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
 // possibly can grab the average star rating from the store
 
 // this component needs to render:
@@ -25,25 +35,63 @@ const SideBar = (props) => {
   const f = Number(recommended.false);
   const percentRecommended = ((t / (t + f)) * 100).toFixed(1);
   // console.log('percent of people recommended', Math.floor(percentRecommended * 100));
-
-
+  const useStyles = makeStyles((theme) => ({
+    progressBar: {
+      flexGrow: 1,
+      height: '15px'
+    },
+  }));
+  const classes = useStyles();
   return (
-    <div>
-      ---------------------------
-      <div>star rating: {averageStarRating.toFixed(1)}</div>
-      <div>{percentRecommended}% of reviews recommend this product</div>
-      <div>5 STARS - {fiveStar}</div>
-      <div>4 STARS - {fourStar}</div>
-      <div>3 STARS - {threeStar}</div>
-      <div>2 STARS - {twoStar}</div>
-      <div>1 STARS - {oneStar}</div>
-      <div>SIZE {characteristics.Fit.value}</div>
-      <div>COMFORT {characteristics.Comfort.value}</div>
-      <div>QUALITY {characteristics.Quality.value}</div>
-      {/* conditionally render the length if it has a length  */}
-      <div>LENGTH {characteristics.Length.value}</div>
-      -----------------------------
-    </div>
+
+    <Grid container spacing={3}>
+      <Grid item><h4>RATINGS & REVIEWS</h4></Grid>
+      <Grid container item>
+        <Grid item>
+          <h1>{averageStarRating.toFixed(1)}</h1>
+        </Grid>
+        <Box component="fieldset" mb={3} borderColor="transparent">
+          <Typography component="legend"></Typography>
+          <Rating
+            name="averageStarRating"
+            value={Number(averageStarRating.toFixed(1))} readOnly
+            precision={0.25}
+            emptyIcon={<StarBorderIcon fontSize="inherit" />}
+          />
+        </Box>
+      </Grid>
+      <Grid item>{percentRecommended}% of reviews recommend this product</Grid>
+      <Grid container item direction="column">
+        <Grid container item direction="row" alignItems="center">
+          5 STARS <LinearProgress className={classes.progressBar} variant="determinate" value={fiveStar} />
+        </Grid>
+        <Grid container item direction="row" alignItems="center">
+          4 STARS<LinearProgress className={classes.progressBar} variant="determinate" value={fourStar} />
+        </Grid>
+        <Grid container item direction="row" alignItems="center">
+          3 STARS<LinearProgress className={classes.progressBar} variant="determinate" value={threeStar} />
+        </Grid>
+        <Grid container item direction="row" alignItems="center">
+          2 STARS<LinearProgress className={classes.progressBar} variant="determinate" value={twoStar} />
+        </Grid>
+        <Grid container item direction="row" alignItems="center">
+          1 STARS<LinearProgress className={classes.progressBar} variant="determinate" value={oneStar} />
+        </Grid>
+      </Grid>
+      <Grid container item direction="column">
+        <Grid container item>SIZE {characteristics.Fit.value}</Grid>
+        <Grid container item>COMFORT {characteristics.Comfort.value}</Grid>
+        <Grid container item>QUALITY {characteristics.Quality.value}</Grid>
+        <Grid container item>LENGTH {characteristics.Length.value}</Grid>
+      </Grid>
+    </Grid>
   );
+
+  // <div>SIZE {characteristics.Fit.value}</div>
+  // <div>COMFORT {characteristics.Comfort.value}</div>
+  // <div>QUALITY {characteristics.Quality.value}</div>
+  // <div>LENGTH {characteristics.Length.value}</div>
+
+
 };
 export default SideBar;
