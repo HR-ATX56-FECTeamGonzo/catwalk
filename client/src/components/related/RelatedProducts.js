fd0d7bb540262c3896bfe3cd47a486f2ca991a70
Jacob Ward <smilinjake@gmail.com>
2021-06-16 00:00:25 -0500

    finally finished the review submission

--- client/src/components/related/RelatedProducts.js
+++ client/src/components/related/RelatedProducts.js
@@ -56,7 +56,7 @@ const RelatedProducts = () => {
 
   useEffect(() => {
     //need to optimize
-    setTimeout(() => setIsLoading(false), 1200);
+    setTimeout(() => setIsLoading(false), 2400);
     getRelatedProductIds();
   }, []);
 
