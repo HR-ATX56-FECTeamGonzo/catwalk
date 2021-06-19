0e6c77bfb1545d2eb08e3fa4817c7f9b1d77818a
Angela Wu <ange.y.wu@gmail.com>
2021-06-19 13:03:46 -0500

    minor edit

--- client/src/components/related/RelatedProducts.js
+++ client/src/components/related/RelatedProducts.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import GITHUB_API_KEY from '../../config/config.js';
 import { Typography } from '@material-ui/core';
 import RPPassProps from './RPPassProps.js';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-
+import ReactDOM from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { updateRelated } from '../../redux-helpers/currentProduct.actions.js';
 
 const RelatedProducts = () => {
   const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hratx';
@@ -14,47 +15,46 @@ const RelatedProducts = () => {
   const [RPInfo, setRPInfo] = useState([]);
   const [RPStyles, setRPStyles] = useState([]);
   const [RPMetaData, setRPMetaData] = useState([]);
+  const dispatch = useDispatch();
+
   const currentProductId = useSelector(state => state.currentProductId);
   const relatedProductIds = useSelector(state => state.related);
 
-  const getRelatedProductIds = () => {
-    axios.get(`${url}/products/${currentProductId}/related`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } })
-      // Promise.resolve(relatedProductIds)
+  const [didChangeIds, setDidChangeIds] = useState(false);
+  const priorRPIds = useRef(relatedProductIds);
+
+  const getRelatedProductIds = (ids) => {
+    //console.log(ids);
+    Promise.resolve(ids)
       .then((result) => {
         let promises = [];
-        for (let i = 0; i < result.data.length; i++) {
+        for (let i = 0; i < ids.length; i++) {
           promises.push(
-            axios.get(`${url}/products/${result.data[i]}`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } })
+            axios.get(`${url}/products/${ids[i]}`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } })
               .then((results) => {
                 setRPInfo(prev => [...prev, results.data]);
               })
           );
         }
-        return Promise.all(promises);
-      })
-      .then((result) => {
-        return axios.get(`${url}/products/${currentProductId}/related`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } });
+        return Promise.resolve(ids);
       })
       .then((result) => {
         let promises = [];
-        for (let i = 0; i < result.data.length; i++) {
+        for (let i = 0; i < result.length; i++) {
           promises.push(
-            axios.get(`${url}/products/${result.data[i]}/styles`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } })
+            axios.get(`${url}/products/${result[i]}/styles`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } })
               .then((results) => {
                 setRPStyles(prev => [...prev, results.data]);
               })
           );
         }
-        return Promise.all(promises);
-      })
-      .then((result) => {
-        return axios.get(`${url}/products/${currentProductId}/related`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } });
+        return Promise.resolve(ids);
       })
       .then((result) => {
         let promises = [];
-        for (let i = 0; i < result.data.length; i++) {
+        for (let i = 0; i < result.length; i++) {
           promises.push(
-            axios.get(`${url}/reviews/meta?product_id=${result.data[i]}`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } })
+            axios.get(`${url}/reviews/meta?product_id=${result[i]}`, { 'headers': { 'Authorization': `${GITHUB_API_KEY}` } })
               .then((results) => {
                 setRPMetaData(prev => [...prev, results.data]);
               })
@@ -69,8 +69,39 @@ const RelatedProducts = () => {
   };
 
   useEffect(() => {
-    getRelatedProductIds();
+    getRelatedProductIds(relatedProductIds);
   }, []);
+
+  useEffect(() => {
+    //console.log('trying to update RP Cards!!!');
+    // if useRef is NOT equal to relatedProductIds
+    let sameRelateds = ((prev, current) => {
+      //console.log('prev: ' + JSON.stringify(prev));
+      //console.log('current: ' + JSON.stringify(current));
+      if (prev.length === current.length) {
+        for (var x = 0; x < prev.length; x++) {
+          if (prev[x] !== current[x]) {
+            return false;
+          }
+        }
+        return true;
+      }
+      return false;
+    })(priorRPIds.current, relatedProductIds);
+
+    //console.log(sameRelateds ? 'related ids are the same' : 'related ids are different');
+    if (!sameRelateds) {
+      //treated as synchronous
+      ReactDOM.unstable_batchedUpdates(() => {
+        setIsLoading(true);
+        setRPInfo([]);
+        setRPStyles([]);
+        setRPMetaData([]);
+      });
+      getRelatedProductIds(relatedProductIds);
+    }
+  }, [relatedProductIds]);
+
   return (
     <div id='relatedProducts'>
       <Typography variant='subtitle1' align='left'>Related Products</Typography>
