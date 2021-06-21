e0fac12177c6174a48ef5d63148739db55acd381
psohn12 <patricksohn12@gmail.com>
2021-06-21 08:37:51 -0500

    api key security (allegedly)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,19 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
+const dotenv = require('dotenv');
 
+module.exports = () => {
+  const env = dotenv.config().parsed;
 
-module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  plugins: [
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-  ]
-});
+  return (merge(common, {
+    mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+      new webpack.DefinePlugin({ 'process.env.API_KEY': JSON.stringify(env.API_KEY) })
+    ]
+  }));
+};
